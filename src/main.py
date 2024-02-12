4001d83fe7e265e51f4eec60a13ee79f6ee94b6b
Izzy Jackson <izzyjackson05@outlook.com>
2024-02-12 01:34:42 +0000

    preferences: remove terminal message when opening preferences

--- src/main.py
+++ src/main.py
@@ -81,7 +81,6 @@ class BinaryApplication(Adw.Application):
 
     def on_preferences_action(self, widget, _):
         """Callback for the app.preferences action."""
-        print('app.preferences action activated')
         prefsWindow = PrefsWindow()
         prefsWindow.set_transient_for(self.props.active_window)
         prefsWindow.present()
