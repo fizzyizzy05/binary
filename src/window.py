399dbd166857d56f437825690e97d846b52ff5ba
Isabelle Jackson <izzyjackson05@outlook.com>
2024-06-30 20:53:16 +0100

    window: Seperate input and output bit counters (part 3)
    Show both bit counters when both bases are in Binary mode

--- src/window.py
+++ src/window.py
@@ -59,7 +59,10 @@ class BinaryWindow(Adw.ApplicationWindow):
         try:
             self.inputHandler()
 
-            if self.in_dropdown.get_selected() == 0:
+            if self.out_dropdown.get_selected() == 0 and self.in_dropdown.get_selected() == 0:
+                self.in_bit_label.set_visible(True)
+                self.out_bit_label.set_visible(True)
+            elif self.in_dropdown.get_selected() == 0:
                 self.in_bit_label.set_visible(True)
                 self.out_bit_label.set_visible(False)
             elif self.out_dropdown.get_selected() == 0:
